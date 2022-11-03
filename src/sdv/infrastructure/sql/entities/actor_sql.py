fd8828e15fa8334e3f49b2e880da0f1ce9a8df41
Micipsa <boukaoumamicipsa@gmail.com>
2022-11-03 16:01:20 +0100

    add myfiles for first time

---
+++ src/sdv/infrastructure/sql/entities/actor_sql.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, VARCHAR, TIMESTAMP
+
+from sdv.infrastructure.sql.entities import Base
+
+
+class Actor(Base):
+    __tablename__ = 'actor'
+
+    id = Column("actor_id", Integer, primary_key=True)
+    first_name = Column("first_name", VARCHAR(45))
+    last_name = Column("last_name", VARCHAR(45))
+    last_update = Column("last_update", TIMESTAMP)
